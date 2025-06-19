fb9b8ba881805e1e48eba7368a21da1746e40439
Mussab <muhammadmussab156@gmail.com>
2025-06-19 17:41:00 +0500

    new cv updated with clean structure

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithub, FaGithubSquare, FaWhatsapp, FaWhatsappSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import profilePic from "@/public/mussab.jpg"; 
+import profilePic from "@/public/mussab.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
